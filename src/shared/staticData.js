222662ac44144fac4cd59474534415d9b6f5ed4e
ahmedelgendy <ahmed.elgendy@ANRPC.LOCAL>
2022-11-09 11:43:01 +0200

    change ip

--- src/shared/staticData.js
+++ src/shared/staticData.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://10.10.6.162:8000";
+export const baseUrl = "http://172.18.8.103:8000";
 export const executedEdara = [
   { label: "تحكم", ID: "850" },
   { label: "كهربا", ID: "800" },
